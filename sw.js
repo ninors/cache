5163a1c9ee1fd71263e7166ecc2e1b1c08d653c3
ninors <143431164+ninors@users.noreply.github.com>
2025-05-06 18:07:07 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,20 @@
-const CACHE_NAME = 'map-cache-v1';
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/leaflet/leaflet.css',
-  '/leaflet/leaflet.js',
-  // Autres fichiers nécessaires, comme ceux que vous pourriez vouloir mettre en cache
-];
-
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        'index.html',
+        'https://unpkg.com/leaflet@1.7.1/dist/leaflet.css',
+        'https://unpkg.com/leaflet@1.7.1/dist/leaflet.js',
+      ]);
+    })
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
